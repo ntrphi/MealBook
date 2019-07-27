1dfed0294e62a81daac5bffa7486aec3e488a90f
ntrphi <48687666+ntrphi@users.noreply.github.com>
2019-07-27 23:50:03 +0700

    add admin theme

---
+++ resources/views/layout/admin_layout.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>AdminLTE 2 | Dashboard</title>
+  <!-- Tell the browser to be responsive to screen width -->
+  <base href="{{asset(' ')}}">
+  <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+    @include('patials.admin.head')
+</head>
+<body class="hold-transition skin-blue sidebar-mini">
+<div class="wrapper">
+
+       @yield('content')
+
+
+  <!-- Control Sidebar -->
+
+  <!-- /.control-sidebar -->
+  <!-- Add the sidebar's background. This div must be placed
+       immediately after the control sidebar -->
+  <div class="control-sidebar-bg"></div>
+</div>
+<!-- ./wrapper -->
+@include('patials.admin.script')
+
+
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 Route::get('/listRecipe', 'CookingRecipeController@getCookingRecipeList');
+Route::prefix('/admin')->group(function () {
+    Route::get('/home', function () {
+        return view('admin.index');
+    });
+});
