f0aee58815e905f87914dd58505d584e7b6f3106
nguyentruongphi155 <nguyentruongphi155@gmail.com>
2019-08-06 14:29:23 +0700

    comment cho rõ

--- app/Http/Controllers/MealBookController.php
+++ app/Http/Controllers/MealBookController.php
@@ -16,14 +16,15 @@ class MealBookController extends Controller
     {
         $user_id=1;
         $name= $request->input('name');
+        // nhận 1 mảng ID của món ăn
         $dishes = $_POST['cookingrecipes'];
-
+        // tạo mâm cơm mới lưu trên bảng meal_book
         $meal_book = new MealBook;
 
         $meal_book->name=$name;
         $meal_book->user_id=$user_id;
         $meal_book->save();
-
+        // lưu mealbook_id và dish_id trên bảng trung gian
         $meal_book->mealBookDishes()->sync($dishes);
 
     }
--- resources/views/frontend/mealbook/add.blade.php
+++ resources/views/frontend/mealbook/add.blade.php
@@ -3,9 +3,12 @@
 <form method="post" action="mealbook-add-save" enctype="multipart/form-data">
 {{ csrf_field() }}   
     <input type="text" placeholder="name" name="name">
-    <input type="checkbox" name="cookingrecipes[]" value="1" checked>
-    <input type="checkbox" name="cookingrecipes[]" value="2" checked>            
-    <input type="checkbox" name="cookingrecipes[]" value="3" checked>            
+    
+    {{-- các id của món ăn đc nhận từ khi kéo thả vào mâm cơm --}}
+    <label for="">món1</label><input type="checkbox" name="cookingrecipes[]" value="1" checked> 
+    <label for="">món2</label><input type="checkbox" name="cookingrecipes[]" value="2" checked>            
+    <label for="">món3</label><input type="checkbox" name="cookingrecipes[]" value="3" checked>  
+
     <button type="submit">Save</button>
 </form>
 @endsection
