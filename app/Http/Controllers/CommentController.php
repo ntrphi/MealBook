5752657626a3e45a03bb5e7dba04d90054bcf813
ntrphi <48687666+ntrphi@users.noreply.github.com>
2019-08-16 23:19:40 +0700

    delete cmt

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -1,6 +1,8 @@
 <?php
 
 namespace App\Http\Controllers;
+
+use App\Comment;
 use App\Post;
 use App\User;
 use App\MealBook;
@@ -12,76 +14,86 @@ use Illuminate\Http\Request;
 class CommentController extends Controller
 {
 
-    public function postComment(Post $post,Request $request){
-      $this->authorize('create');	
+   public function postComment(Post $post, Request $request)
+   {
+      $this->authorize('create');
       $rules = [
          'title' => 'required | min:4',
          'content' => 'required | min: 4',
       ];
-   
+
       $msg = [
          'required' => ':attribute không được bỏ trống.',
          'min' => ':attribute quá ngắn',
-     ];
-   
-      $validator = Validator::make($request->all(), $rules , $msg);
-   
-      if ($validator->fails()) {
-           return redirect()->back()
-                       ->withErrors($validator)
-                       ->withInput();
-       }else{
+      ];
 
-         $post->comment()->create(['user_id'=>Auth::user()->id,'title'=>$request->title,'content'=>$request->content,]);
+      $validator = Validator::make($request->all(), $rules, $msg);
+
+      if ($validator->fails()) {
+         return redirect()->back()
+            ->withErrors($validator)
+            ->withInput();
+      } else {
+
+         $post->comment()->create(['user_id' => Auth::user()->id, 'title' => $request->title, 'content' => $request->content,]);
          return back();
       }
-    }
-    public function cookingComment(CookingRecipe $cooking,Request $request){
+   }
+   public function cookingComment(CookingRecipe $cooking, Request $request)
+   {
       $rules = [
          'title' => 'required | min:4',
          'content' => 'required | min: 4',
       ];
-   
+
       $msg = [
          'required' => ':attribute không được bỏ trống.',
          'min' => ':attribute quá ngắn',
-     ];
-   
-      $validator = Validator::make($request->all(), $rules , $msg);
-   
-      if ($validator->fails()) {
-           return redirect()->back()
-                       ->withErrors($validator)
-                       ->withInput();
-       }else{
+      ];
 
-   
-        $cooking->comment()->create(['user_id'=>Auth::user()->id,'title'=>$request->title,'content'=>$request->content,]);
-        return back();
-       }
-     }
-     public function mealbookComment(MealBook $mealbook,Request $request){
+      $validator = Validator::make($request->all(), $rules, $msg);
+
+      if ($validator->fails()) {
+         return redirect()->back()
+            ->withErrors($validator)
+            ->withInput();
+      } else {
+
+
+         $cooking->comment()->create(['user_id' => Auth::user()->id, 'title' => $request->title, 'content' => $request->content,]);
+         return back();
+      }
+   }
+   public function mealbookComment(MealBook $mealbook, Request $request)
+   {
 
       $rules = [
          'title' => 'required | min:4',
          'content' => 'required | min: 4',
       ];
-   
+
       $msg = [
          'required' => ':attribute không được bỏ trống.',
          'min' => ':attribute quá ngắn',
-     ];
-   
-      $validator = Validator::make($request->all(), $rules , $msg);
-   
-      if ($validator->fails()) {
-           return redirect()->back()
-                       ->withErrors($validator)
-                       ->withInput();
-       }else{
+      ];
 
-      $mealbook->comment()->create(['user_id'=>Auth::user()->id,'title'=>$request->title,'content'=>$request->content,]);
+      $validator = Validator::make($request->all(), $rules, $msg);
+
+      if ($validator->fails()) {
+         return redirect()->back()
+            ->withErrors($validator)
+            ->withInput();
+      } else {
+
+         $mealbook->comment()->create(['user_id' => Auth::user()->id, 'title' => $request->title, 'content' => $request->content,]);
+         return back();
+      }
+   }
+   public function delete(Request $request)
+   {
+      $id = $request->id;
+      $comment = Comment::find($id);
+      $comment->delete();
       return back();
-       }
    }
 }
