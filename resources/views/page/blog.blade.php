9a13fa21fc5e7bc07d58a1b28080bc17d41eae75
idolz <vitc0nls94@gmail.com>
2019-08-16 22:37:24 +0700

    edit post

--- resources/views/page/blog.blade.php
+++ resources/views/page/blog.blade.php
@@ -113,7 +113,9 @@
                       </form>
 									</div>
 							</div>
-							<div class="col-lg-4">
+					
+            </div>
+            <div class="col-lg-4">
                 <div class="blog_right_sidebar">
                       <aside class="single_sidebar_widget search_widget">
                           <form action="#">
@@ -142,89 +144,11 @@
                               </div>
                           </div>
                 @endforeach
-                     
-                      </aside>
-                      <!-- <aside class="single_sidebar_widget tag_cloud_widget">
-                          <h4 class="widget_title">Tag Clouds</h4>
-                          <ul class="list">
-                              <li>
-                                  <a href="#">project</a>
-                              </li>
-                              <li>
-                                  <a href="#">love</a>
-                              </li>
-                              <li>
-                                  <a href="#">technology</a>
-                              </li>
-                              <li>
-                                  <a href="#">travel</a>
-                              </li>
-                              <li>
-                                  <a href="#">restaurant</a>
-                              </li>
-                              <li>
-                                  <a href="#">life style</a>
-                              </li>
-                              <li>
-                                  <a href="#">design</a>
-                              </li>
-                              <li>
-                                  <a href="#">illustration</a>
-                              </li>
-                          </ul>
-                      </aside>
-
-
-                      <aside class="single_sidebar_widget instagram_feeds">
-                        <h4 class="widget_title">Instagram Feeds</h4>
-                        <ul class="instagram_row flex-wrap">
-                            <li>
-                                <a href="#">
-                                  <img class="img-fluid" src="images/instagram/widget-i1.png" alt="">
-                                </a>
-                            </li>
-                            <li>
-                                <a href="#">
-                                  <img class="img-fluid" src="images/instagram/widget-i2.png" alt="">
-                                </a>
-                            </li>
-                            <li>
-                                <a href="#">
-                                  <img class="img-fluid" src="images/instagram/widget-i3.png" alt="">
-                                </a>
-                            </li>
-                            <li>
-                                <a href="#">
-                                  <img class="img-fluid" src="images/instagram/widget-i4.png" alt="">
-                                </a>
-                            </li>
-                            <li>
-                                <a href="#">
-                                  <img class="img-fluid" src="images/instagram/widget-i5.png" alt="">
-                                </a>
-                            </li>
-                            <li>
-                                <a href="#">
-                                  <img class="img-fluid" src="images/instagram/widget-i6.png" alt="">
-                                </a>
-                            </li>
-                        </ul>
-                      </aside>
-
-
-                      <aside class="single_sidebar_widget newsletter_widget">
-                        <h4 class="widget_title">Newsletter</h4>
-
-                        <form action="#">
-                          <div class="form-group">
-                            <input type="email" class="form-control" placeholder="Enter email" required>
-                          </div>
-                          <button class="button rounded-0 primary-bg text-white w-100" type="submit">Subscribe</button>
-                        </form>
-                      </aside> -->
+                   </aside>
                   </div>
 							</div>
 					</div>
+          </div>
 			</div>
 	</section>
 
